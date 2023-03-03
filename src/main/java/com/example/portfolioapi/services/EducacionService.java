e78d431b89bf7b5af8b9f84d23f29dd0f97b6565
cil4 <masham1860@gmail.com>
2023-03-03 11:39:49 -0300

    cambios

---
+++ src/main/java/com/example/portfolioapi/services/EducacionService.java
@@ -0,0 +1,45 @@
+package com.example.portfolioapi.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.portfolioapi.models.EducacionModel;
+import com.example.portfolioapi.repositories.EducacionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EducacionService {
+    @Autowired
+    EducacionRepository educaRepo;
+
+  
+
+    public ArrayList<EducacionModel>obtenerEducacion(){
+        return (ArrayList<EducacionModel>) educaRepo.findAll();
+    }
+    public EducacionModel guardarEducacion(EducacionModel educacion){
+        return educaRepo.save(educacion);
+    }
+    public EducacionModel editarEducacion(EducacionModel educacion){
+        return educaRepo.save(educacion);
+    }
+    public Optional<EducacionModel>obtenerEducacionPorId(Integer id){
+        return educaRepo.findById(id);
+    }
+    
+    public boolean existsById(Integer id){
+        return educaRepo.existsById(id);
+    }
+    public boolean borrarEducacion(Integer id){
+        try{
+            educaRepo.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/example/portfolioapi/services/ExperienciaService.java
@@ -0,0 +1,45 @@
+package com.example.portfolioapi.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.portfolioapi.models.ExperienciaModel;
+import com.example.portfolioapi.repositories.ExperienciaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExperienciaService {
+    @Autowired
+    ExperienciaRepository expeRepo;
+
+   
+
+    public ArrayList<ExperienciaModel> obtenerExperiencia(){
+        return (ArrayList<ExperienciaModel>) expeRepo.findAll();
+        }
+        public ExperienciaModel guardarExperiencia(ExperienciaModel experiencia){
+            return expeRepo.save(experiencia);
+        }
+        public ExperienciaModel editarExperiencia(ExperienciaModel experiencia){
+            return expeRepo.save(experiencia);
+        }
+        public Optional<ExperienciaModel> obtenerExperienciaById(Integer id){
+            return expeRepo.findById(id);
+        }
+        public boolean existsById(Integer id){
+            return expeRepo.existsById(id);
+        }
+    
+        public boolean borrarExperiencia(Integer id){
+            try{
+                expeRepo.deleteById(id);
+                return true;
+            }catch(Exception err){
+                return false;
+            }
+        }
+}
